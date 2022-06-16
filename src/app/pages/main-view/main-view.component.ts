cd2054a1db9c4124e4aed8bd52b0aa115860eed2
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-06-16 21:59:41 +0800

    model

--- src/app/pages/main-view/main-view.component.ts
+++ src/app/pages/main-view/main-view.component.ts
@@ -4,6 +4,8 @@ import {
   moveItemInArray,
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
+import { Board } from 'src/app/models/board.model';
+import { Column } from 'src/app/models/column.model';
 
 @Component({
   selector: 'app-main-view',
@@ -13,11 +15,19 @@ import {
 export class MainViewComponent implements OnInit {
   constructor() {}
 
+  board: Board = new Board('Test board', [
+    new Column('Ideas', ['Some random idea']),
+    new Column('Research', ['Research 1', 'Research 2', 'Research 3']),
+    new Column('Todo', [
+      'Get to work',
+      'Pick up groceries',
+      'Go home',
+      'Fall asleep',
+    ]),
+    new Column('Done', ['Get up', 'Brush teeth', 'take a shower']),
+  ]);
+
   ngOnInit(): void {}
-
-  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
-
-  done = ['Get up', 'Brush teeth', 'take a shower'];
 
   drop(event: CdkDragDrop<string[]>) {
     if (event.previousContainer === event.container) {
